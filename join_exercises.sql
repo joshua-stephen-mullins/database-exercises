6b76e5316bf10e576f36404a8a9fd030607df54f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-01-25 12:17:55 -0600

    completed subqueries exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,41 +1,41 @@
-# use join_test_db;
-
-# CREATE TABLE roles (
-#                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#                        name VARCHAR(100) NOT NULL,
-#                        PRIMARY KEY (id)
-# );
-#
-# CREATE TABLE users (
-#                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#                        name VARCHAR(100) NOT NULL,
-#                        email VARCHAR(100) NOT NULL,
-#                        role_id INT UNSIGNED DEFAULT NULL,
-#                        PRIMARY KEY (id),
-#                        FOREIGN KEY (role_id) REFERENCES roles (id)
-# );
-#
-# INSERT INTO roles (name) VALUES ('admin');
-# INSERT INTO roles (name) VALUES ('author');
-# INSERT INTO roles (name) VALUES ('reviewer');
-# INSERT INTO roles (name) VALUES ('commenter');
-#
-# INSERT INTO users (name, email, role_id) VALUES
-#                                              ('bob', 'bob@example.com', 1),
-#                                              ('joe', 'joe@example.com', 2),
-#                                              ('sally', 'sally@example.com', 3),
-#                                              ('adam', 'adam@example.com', 3),
-#                                              ('jane', 'jane@example.com', null),
-#                                              ('mike', 'mike@example.com', null);
-
-
-# SELECT users.name as user_name, roles.name as role_name
-# FROM roles
-#          LEFT JOIN users ON users.role_id = roles.id;
-#
-# SELECT users.name AS user_name, roles.name AS role_name
-# FROM users
-#         JOIN roles ON users.role_id = roles.id;
+-- # use join_test_db;
+--
+-- # CREATE TABLE roles (
+-- #                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+-- #                        name VARCHAR(100) NOT NULL,
+-- #                        PRIMARY KEY (id)
+-- # );
+-- #
+-- # CREATE TABLE users (
+-- #                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+-- #                        name VARCHAR(100) NOT NULL,
+-- #                        email VARCHAR(100) NOT NULL,
+-- #                        role_id INT UNSIGNED DEFAULT NULL,
+-- #                        PRIMARY KEY (id),
+-- #                        FOREIGN KEY (role_id) REFERENCES roles (id)
+-- # );
+-- #
+-- # INSERT INTO roles (name) VALUES ('admin');
+-- # INSERT INTO roles (name) VALUES ('author');
+-- # INSERT INTO roles (name) VALUES ('reviewer');
+-- # INSERT INTO roles (name) VALUES ('commenter');
+-- #
+-- # INSERT INTO users (name, email, role_id) VALUES
+-- #                                              ('bob', 'bob@example.com', 1),
+-- #                                              ('joe', 'joe@example.com', 2),
+-- #                                              ('sally', 'sally@example.com', 3),
+-- #                                              ('adam', 'adam@example.com', 3),
+-- #                                              ('jane', 'jane@example.com', null),
+-- #                                              ('mike', 'mike@example.com', null);
+--
+--
+-- # SELECT users.name as user_name, roles.name as role_name
+-- # FROM roles
+-- #          LEFT JOIN users ON users.role_id = roles.id;
+-- #
+-- # SELECT users.name AS user_name, roles.name AS role_name
+-- # FROM users
+-- #         JOIN roles ON users.role_id = roles.id;
 
 use employees;
 
@@ -76,4 +76,18 @@ FROM employees as e
          JOIN salaries as s
               ON s.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01' and s.to_date = '9999-01-01' order by s.salary;
+
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_emp
+    WHERE  dept_no IN (
+        SELECT dept_no
+        FROM departments
+        WHERE dept_name = 'Development'
+        )
+    )
+AND MONTH(birth_date) = 12 AND DAY(birth_date) = 28;
 
